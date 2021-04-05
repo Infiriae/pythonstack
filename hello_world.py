711d55b8cfcc3f65ee00c59bed9b062280d12705
Infiriae <kirkduhon@gmail.com>
2021-04-05 10:29:21 -0500

    Adding base python repo + first assignment

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+n = "Kirk"
+
+print("Hello", n,"!")
+
+print("Hello "+n+"!")
+
+x = 7
+
+print("Hello",x,"!")
+
+print("Hello "+str(x)+"!")
+
+foo1 = "hot wings"
+foo2 = "coconut curry"
+
+print("I love to eat {} and {}.".format(foo1,foo2))
+
+print(f"I love to eat {foo1} and {foo2}.")
+
+print(f'I love to eat %s and %s.' % (foo1, foo2))
