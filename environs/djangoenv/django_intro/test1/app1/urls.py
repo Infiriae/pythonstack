119df41f3dc84ec82f6b61dbceca8271927fa79b
Infiriae <kirkduhon@gmail.com>
2021-04-26 12:05:50 -0500

    completed blogs assignment

--- environs/djangoenv/django_intro/test1/app1/urls.py
+++ environs/djangoenv/django_intro/test1/app1/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views	# the . indicates that the views file can be found in the same directory as this file
                     
 urlpatterns = [
-    path('index', views.index),
+    path('', views.index),
+    path('new',views.new),
+    path('create',views.create),
+    path('<int:num>',views.show),
+    path('<int:num>/edit',views.edit),
+    path('<int:num>/delete',views.edit),
     path('bears', views.one_method),                        # would only match localhost:8000/bears
     path('bears/<int:my_val>', views.another_method),       # would match localhost:8000/bears/23
     path('bears/<str:name>/poke', views.yet_another),       # would match localhost:8000/bears/pooh/poke
--- environs/djangoenv/django_intro/test1/app1/views.py
+++ environs/djangoenv/django_intro/test1/app1/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 def index(request):
-    
-    return HttpResponse("<a href=\"/index\">I</a> am ready to handle a request for '/'! <div><ul><li><form label=\"name\"><input text=\"Bear?\" id=\"bears\"><submit label=\"name\" text=\"Bears Name\"></form></li><li>Ok</li></ul></div>")
+    return HttpResponse("Placeholder to later display all blogs. <div><a href=\"/new\">To New placeholder</a></div><div><a href=\"/15\">To show 15 placeholder</a></div><div><a href=\"/15/edit\">To edit 15 placeholder</a></div>")
+    #return HttpResponse("<a href=\"/index\">I</a> am ready to handle a request for '/'! <div><ul><li><form label=\"name\"><input text=\"Bear?\" id=\"bears\"><submit label=\"name\" text=\"Bears Name\"></form></li><li>Ok</li></ul></div>")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog<div><a href=\"/\">To home placeholder</a></div>")
+
+def create(request):
+    return redirect('/')
+
+def show(request, num):
+    return HttpResponse(f"placeholder to display blog number: {num}<div><a href=\"/\">To home placeholder</a></div>")
+
+def edit(request, num):
+    return HttpResponse(f"placeholder to edit blog number: {num}<div><a href=\"/\">To home placeholder</a></div>")
+
+def destroy(request):
+    return redirect('/')
 
 def one_method(request):                #no values passed via URL
     return HttpResponse("I have a bear")
